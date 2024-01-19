e9e523b3c5c0639f0864c898f85998b760673762
Eugene-prog <ieugene.prog@gmail.com>
2024-01-19 22:45:35 +0700

    Что такое валидация? [#246864]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -42,7 +42,7 @@ public class Tracker {
 
     public void delete(int id) {
         int index = indexOf(id);
-        if (index == -1) {
+        if (index == -1 || id < 0) {
             return;
         }
         System.arraycopy(items, index, items, index + 1, size - index - 1);
