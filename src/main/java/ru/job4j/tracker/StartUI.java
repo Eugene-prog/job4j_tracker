efa82c74f08e97f70e97c90ae59cbee286586c56
Eugene-prog <ieugene.prog@gmail.com>
2024-01-20 19:37:23 +0700

    toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -8,5 +8,7 @@ public class StartUI {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String currentDateTimeFormat = item.getCreated().format(formatter);
         System.out.println(currentDateTimeFormat);
+        Item item1 = new Item();
+        System.out.println(item1);
     }
 }
