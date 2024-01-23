c44f7e877ed83d6a9de98894f31ba34510f043ee
Eugene-prog <ieugene.prog@gmail.com>
2024-01-23 22:18:43 +0700

    Input и полиморфизм. [#181094]

--- src/main/java/ru/job4j/tracker/MockInput.java
+++ src/main/java/ru/job4j/tracker/MockInput.java
@@ -1,14 +1,20 @@
 package ru.job4j.tracker;
 
 public class MockInput implements Input {
+    private String[] answers;
+    private int position = 0;
+
+    public MockInput(String[] answers) {
+        this.answers = answers;
+    }
 
     @Override
     public String askStr(String question) {
-        return null;
+        return answers[position++];
     }
 
     @Override
     public int askInt(String question) {
-        return 0;
+        return Integer.parseInt(askStr(question));
     }
 }
---
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -0,0 +1,18 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StartUITest {
+    @Test
+    void whenAddItem() {
+        String[] answers = {"Fix PC"};
+        Input input = new MockInput(answers);
+        Tracker tracker = new Tracker();
+        StartUI.createItem(input, tracker);
+        Item created = tracker.findAll()[0];
+        Item expected = new Item("Fix PC");
+        assertThat(created.getName()).isEqualTo(expected.getName());
+    }
+}
