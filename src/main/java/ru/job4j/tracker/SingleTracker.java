dcf6093e178070c7f0f1bb89aec9c5d7b1520609
Eugene-prog <ieugene.prog@gmail.com>
2024-02-03 19:44:38 +0700

    Singleton [#94606]

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,42 @@
+package ru.job4j.tracker;
+
+public class SingleTracker {
+
+    private static SingleTracker instance;
+    private final Tracker tracker;
+
+    private SingleTracker() {
+        tracker = new Tracker();
+    }
+
+    public static SingleTracker getInstance() {
+        if (instance == null) {
+            instance = new SingleTracker();
+        }
+        return instance;
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public void delete(int id) {
+        tracker.delete(id);
+    }
+
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+}
