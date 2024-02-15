9d144f319f9e9ad1af3fa53d055f847471249e5a
Eugene-prog <ieugene.prog@gmail.com>
2024-02-15 20:43:46 +0700

    fix Идентичные тексты [#212655 #481214]

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -5,15 +5,14 @@ import java.util.HashSet;
 
 public class UniqueText {
     public boolean isEquals(String originText, String duplicateText) {
-        boolean result = true;
         String[] origin = originText.split(" ");
         String[] text = duplicateText.split(" ");
         HashSet<String> check = new HashSet<>(Arrays.asList(origin));
         for (String word : text) {
             if (!check.contains(word)) {
-                return !result;
+                return false;
             }
         }
-        return result;
+        return true;
     }
 }
