0b69abed2993ad98783fb436f2c27aca97ce0888
Eugene-prog <ieugene.prog@gmail.com>
2024-02-17 20:22:49 +0700

    Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("ivanov@mail.ru", "Ivan Ivanov");
+        map.put("petrov@mail.ru", "Petr Petrov");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " " + value);
+        }
+    }
+}
