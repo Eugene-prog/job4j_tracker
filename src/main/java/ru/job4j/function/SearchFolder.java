13a29fc51b2573379635a257d42c67ed91b886d0
Eugene-prog <ieugene.prog@gmail.com>
2024-03-02 21:45:56 +0700

    Встроенные функциональные интерфейсы. [#249209]

---
+++ src/main/java/ru/job4j/function/SearchFolder.java
@@ -0,0 +1,25 @@
+package ru.job4j.function;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class SearchFolder {
+    public static List<Folder> filterSize(List<Folder> list) {
+        return filter(list, folder -> folder.getSize() > 100);
+    }
+
+    public static List<Folder> filterName(List<Folder> list) {
+        return filter(list, folder -> folder.getName().contains("bug"));
+    }
+
+    public static List<Folder> filter(List<Folder> list, Predicate<Folder> predicate) {
+        List<Folder> result = new ArrayList<>();
+        for (Folder folder : list) {
+            if (predicate.test(folder)) {
+                result.add(folder);
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/java/ru/job4j/function/SearchFolderTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.function;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SearchFolderTest {
+    @Test
+    public void whenFilterSize() {
+        List<Folder> list = List.of(
+                new Folder("fix", 110),
+                new Folder("bug", 75),
+                new Folder("bug", 90)
+        );
+        List<Folder> expected = List.of(new Folder("fix", 110));
+        Predicate<Folder> predicate = folder -> folder.getSize() > 100;
+        List<Folder> result = SearchFolder.filter(list, predicate);
+        assertThat(result).containsAll(expected);
+    }
+
+    @Test
+    public void whenFilterName() {
+        List<Folder> list = List.of(
+                new Folder("fix", 110),
+                new Folder("bug", 75),
+                new Folder("bug", 90)
+        );
+        List<Folder> expected = List.of(
+                new Folder("bug", 75),
+                new Folder("bug", 90)
+        );
+        Predicate<Folder> predicate = folder -> folder.getName().contains("bug");
+        List<Folder> result = SearchFolder.filter(list, predicate);
+        System.out.println(result);
+        assertThat(result).containsAll(expected);
+    }
+}
