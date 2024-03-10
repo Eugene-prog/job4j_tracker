c2f40cf9ca367721910aa5467b040919a48352d0
Eugene-prog <ieugene.prog@gmail.com>
2024-03-10 13:02:20 +0700

    fix Фильтрация учеников [#110058]

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -6,6 +6,9 @@ import java.util.stream.Collectors;
 
 public class School {
     public List<Student> collect(List<Student> students, Predicate<Student> predicate) {
-        return students.stream().filter(predicate).collect(Collectors.toList());
+        return students
+                .stream()
+                .filter(predicate)
+                .collect(Collectors.toList());
     }
 }
