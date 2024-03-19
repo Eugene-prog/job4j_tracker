04e29c872e18bf8ca5cb19a6a13302a18d2a11bb
Eugene-prog <ieugene.prog@gmail.com>
2024-03-19 21:24:34 +0700

    fix Stream API улучшения [#58167 #486671]

--- src/main/java/ru/job4j/stream/StudentLevel.java
+++ src/main/java/ru/job4j/stream/StudentLevel.java
@@ -7,7 +7,7 @@ public class StudentLevel {
     public static List<Student> levelOf(List<Student> students, int bound) {
         return students.stream()
                 .filter(student -> student != null)
-                .sorted((left, right) -> (right.getScore() - left.getScore()))
+                .sorted((left, right) -> Integer.compare(right.getScore(), (left.getScore())))
                 .takeWhile(student -> student.getScore() >= bound)
                 .collect(Collectors.toList());
     }
